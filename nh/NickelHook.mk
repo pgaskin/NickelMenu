dc7334b9cd86a801b4949fa675dc025b7c068170
Patrick Gaskin <patrick@pgaskin.net>
2020-07-21 17:02:06 -0400

    Added NickelHook project generator

--- nh/NickelHook.mk
+++ nh/NickelHook.mk
@@ -1,18 +1,15 @@
 # GNU Makefile include for NickelHook plugins
 
-# include NickelHook.mk
-# ...variables
-# ...custom rules
-# include NickelHook.mk
-
-# comma literal
+# literals
+override nh_parl := (
+override nh_parr := )
 override nh_comma := ,
-
-# newline literal
 override define nh_newline
 
 
 endef
+
+ifneq ($(firstword $(MAKEFILE_LIST)),$(lastword $(MAKEFILE_LIST)))
 
 ifndef nh_top
 CROSS_COMPILE  = arm-nickel-linux-gnueabihf-
@@ -198,3 +195,75 @@ clangd:
 endif
 
 override nh_top := true
+override nh_lib := true
+endif
+
+# generator
+ifndef nh_lib
+$(if $(NAME),,$(error Please specify NAME (ex: MyMod)))
+
+override nh_namet := $(shell echo "$(NAME)"     | tr -d ' [:lower:]')
+override nh_nameu := $(shell echo "$(nh_namet)" | tr '[:upper:]' '[:lower:]')
+override nh_names := $(shell echo "$(NAME)"     | tr -d ' ')
+override nh_namel := $(shell echo "$(nh_names)" | tr '[:upper:]' '[:lower:]')
+
+$(if $(nh_namet),,$(error NAME must contain at least one uppercase letter, preferably more to be unique))
+
+LIBRARY  ?= lib$(nh_namel).so
+NHSOURCE ?= src/$(patsubst lib%,%,$(patsubst %.so,%,$(LIBRARY))).cc
+SOURCES  ?= $(wildcard src/*.c src/*.cc)
+
+override nh_dir    := $(dir $(lastword $(MAKEFILE_LIST)))
+override nh_mkdir   = @echo "$@"; mkdir $@
+override nh_create  = @echo "$@"; echo > $@
+override nh_write   = @echo "| $(subst ",\",$(1))"; echo "$(subst ",\",$(1))" >> $@
+
+$(if $(wildcard $(NHSOURCE)),$(info Warning: File named $(NHSOURCE) found in src dir, not generating NickelHook base.))
+$(if $(wildcard Makefile),$(info Warning: Makefile already exists, not generating.))
+
+all: Makefile src $(NHSOURCE)
+	@echo "Finishing up."
+	make gitignore
+.PHONY: all
+
+Makefile:
+	$(call nh_create)
+	$(call nh_write,include $(nh_dir)NickelHook.mk)
+	$(call nh_write,)
+	$(call nh_write,override LIBRARY  := $(LIBRARY))
+	$(call nh_write,override SOURCES  += $(NHSOURCE) $(sort $(filter-out $(NHSOURCE), $(SOURCES))))
+	$(call nh_write,override CFLAGS   += -Wall -Wextra -Werror)
+	$(call nh_write,override CXXFLAGS += -Wall -Wextra -Werror -Wno-missing-field-initializers)
+	$(call nh_write,)
+	$(call nh_write,include $(nh_dir)NickelHook.mk)
+
+src:
+	$(call nh_mkdir)
+
+$(NHSOURCE): src
+	$(call nh_create)
+	$(call nh_write,#include <cstddef>)
+	$(call nh_write,#include <cstdlib>)
+	$(call nh_write,)
+	$(call nh_write,#include <NickelHook.h>)
+	$(call nh_write,)
+	$(call nh_write,static struct nh_info $(nh_names) = {)
+	$(call nh_write,    .name           = "$(NAME)"$(nh_comma))
+	$(call nh_write,    .desc           = ""$(nh_comma))
+	$(call nh_write,    .uninstall_flag = "/mnt/onboard/$(nh_namet)_uninstall"$(nh_comma))
+	$(call nh_write,};)
+	$(call nh_write,static struct nh_hook $(nh_names)Hook[] = {)
+	$(call nh_write,    {0}$(nh_comma))
+	$(call nh_write,};)
+	$(call nh_write,)
+	$(call nh_write,static struct nh_dlsym $(nh_names)Dlsym[] = {)
+	$(call nh_write,    {0}$(nh_comma))
+	$(call nh_write,};)
+	$(call nh_write,)
+	$(call nh_write,NickelHook$(nh_parl))
+	$(call nh_write,    .init  = nullptr$(nh_comma))
+	$(call nh_write,    .info  = &$(nh_names)$(nh_comma))
+	$(call nh_write,    .hook  = $(nh_names)Hook$(nh_comma))
+	$(call nh_write,    .dlsym = $(nh_names)Dlsym$(nh_comma))
+	$(call nh_write,$(nh_parr))
+endif
--- nh/README.md
+++ nh/README.md
@@ -4,6 +4,7 @@ A robust library for creating Nickel mods.
 
 **Features:**
 - Generates a Qt plugin designed for use with Nickel.
+- Project generator.
 - C library:
   - Failsafe mechanism.
   - Uninstall when a path exists.
@@ -18,7 +19,7 @@ A robust library for creating Nickel mods.
   - Designed for use with [NickelTC](https://github.com/pgaskin/NickelTC).
   - pkg-config integration.
   - Supports C, C++, Qt MOC.
-  - clangd compile_flags.json generation.
+  - compile_flags.json generation for clangd or other tools.
   - KoboRoot.tgz generation with custom files.
   - Can also install to a DESTDIR instead.
   - Supports custom CPPFLAGS/CFLAGS/CXXFLAGS/LDFLAGS.
@@ -27,6 +28,11 @@ A robust library for creating Nickel mods.
   - Supports custom targets.
 
 ## Installation
-Copy this folder to your project, or include it as a git submodule.
+
+1. Copy this folder to your project, or include it as a git submodule.
+2. Run `make -f ./relative/path/to/NickelHook.mk NAME=MyModName`.
+3. Build using `make`. To generate a KoboRoot.tgz, run `make koboroot` after building. To clean, run `make clean`.
+4. Whenever you change header files, run `make clean`. If you add/remove source files, run `make gitignore`.
+5. To generate a compile_commands.json, use `make clangd CROSS_COMPILE= CC=clang-10 CXX=clang++-10 CFLAGS= CXXFLAGS=`.
 
 <!-- TODO: usage -->
