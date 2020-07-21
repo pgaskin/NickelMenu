pgaskin/NickelMenu

commit 6329328daf09b50e18bbd4dad35cc7e8e36f7f78

Refactored error handling (#62)
Instead of using and checking malloc'd error strings formatted with asprintf and passed through a pointer in function arguments, use a global thread-local statically allocated error buffer with functions to set and get it.

This is more efficient, less error-prone, and easier to use than the old method, especially now that NM is larger (meaning that errors are returned through multiple layers) and parts of it are re-used in other things.

src/config.c
@@ -48,14 +48,12 @@ static int nm_config_files_filter(const struct dirent *de) {
     return 1;
 }
 
-nm_config_file_t *nm_config_files(char **err_out) {
-    #define NM_ERR_RET NULL
-
+nm_config_file_t *nm_config_files() {
     nm_config_file_t *cfs = NULL, *cfc = NULL;
 
     struct dirent **nl;
     int n = scandir(NM_CONFIG_DIR, &nl, nm_config_files_filter, alphasort);
-    NM_ASSERT(n != -1, "could not scan config dir: %s", strerror(errno));
+    NM_CHECK(NULL, n != -1, "could not scan config dir: %s", strerror(errno));
 
     for (int i = 0; i < n; i++) {
         struct dirent *de = nl[i];
@@ -72,9 +70,9 @@ nm_config_file_t *nm_config_files(char **err_out) {
                 cfs = tmp;
             }
             if (!fn)
-                NM_RETURN_ERR("could not build full path for config file");
+                NM_ERR_RET(NULL, "could not build full path for config file");
             free(fn);
-            NM_RETURN_ERR("could not stat %s/%s", NM_CONFIG_DIR, de->d_name);
+            NM_ERR_RET(NULL, "could not stat %s/%s", NM_CONFIG_DIR, de->d_name);
         }
 
         // skip it if it isn't a file
@@ -100,21 +98,19 @@ nm_config_file_t *nm_config_files(char **err_out) {
 
     free(nl);
 
-    NM_RETURN_OK(cfs);
-    #undef NM_ERR_RET
+    return cfs;
 }
 
-bool nm_config_files_update(nm_config_file_t **files, char **err_out) {
-    #define NM_ERR_RET false
-    NM_ASSERT(files, "files pointer must not be null");
+int nm_config_files_update(nm_config_file_t **files) {
+    NM_CHECK(false, files, "files pointer must not be null");
 
-    nm_config_file_t *nfiles = nm_config_files(err_out);
-    if (*err_out)
-        return NM_ERR_RET;
+    nm_config_file_t *nfiles = nm_config_files();
+    if (nm_err_peek())
+        return -1; // the error is passed on
 
     if (!*files) {
         *files = nfiles;
-        NM_RETURN_OK(true);
+        return 0;
     }
 
     bool ch = false;
@@ -133,13 +129,11 @@ bool nm_config_files_update(nm_config_file_t **files, char **err_out) {
     if (ch || op || np) {
         nm_config_files_free(*files);
         *files = nfiles;
-        NM_RETURN_OK(true);
+        return 0;
     } else {
         nm_config_files_free(nfiles);
-        NM_RETURN_OK(false);
+        return 1;
     }
-
-    #undef NM_ERR_RET
 }
 
 void nm_config_files_free(nm_config_file_t *files) {
@@ -206,20 +200,18 @@ static const char* nm_config_parse__strerror(nm_config_parse__append__ret_t ret)
 
 // note: line must point to the part after the config line type, and will be
 //       modified. if the config line type doesn't match, everything will be
-//       left as-is and false will be returned without an error. if an error
-//       occurs, err_out will be set and true will be returned (since stuff
-//       may be modified). otherwise, true is returned without an error (the
+//       left as-is and false will be returned with nm_err cleared. if an error
+//       occurs, nm_err will be set and true will be returned (since stuff
+//       may be modified). otherwise, true is returned with nm_err cleared (the
 //       parsed output will have strings pointing directly into the line).
 
-static bool nm_config_parse__lineend_action(int field, char **line, bool p_on_success, bool p_on_failure, nm_menu_action_t *act_out, char **err_out);
-static bool nm_config_parse__line_item(const char *type, char **line, nm_menu_item_t *it_out, nm_menu_action_t *action_out, char **err_out);
-static bool nm_config_parse__line_chain(const char *type, char **line, nm_menu_action_t *act_out, char **err_out);
-static bool nm_config_parse__line_generator(const char *type, char **line, nm_generator_t *gn_out, char **err_out);
+static bool nm_config_parse__lineend_action(int field, char **line, bool p_on_success, bool p_on_failure, nm_menu_action_t *act_out);
+static bool nm_config_parse__line_item(const char *type, char **line, nm_menu_item_t *it_out, nm_menu_action_t *action_out);
+static bool nm_config_parse__line_chain(const char *type, char **line, nm_menu_action_t *act_out);
+static bool nm_config_parse__line_generator(const char *type, char **line, nm_generator_t *gn_out);
 
-nm_config_t *nm_config_parse(nm_config_file_t *files, char **err_out) {
-    #define NM_ERR_RET NULL
-
-    char *err = NULL;
+nm_config_t *nm_config_parse(nm_config_file_t *files) {
+    const char *err = NULL;
 
     FILE   *cfgfile    = NULL;
     char   *line       = NULL;
@@ -237,12 +229,9 @@ nm_config_t *nm_config_parse(nm_config_file_t *files, char **err_out) {
     #define RETERR(fmt, ...) do {                  \
         if (cfgfile)                               \
             fclose(cfgfile);                       \
-        if (err_out)                               \
-            asprintf(err_out, fmt, ##__VA_ARGS__); \
-        free(err);                                 \
         free(line);                                \
         nm_config_free(state.cfg_c);               \
-        return NM_ERR_RET;                         \
+        NM_ERR_RET(NULL, fmt, ##__VA_ARGS__);      \
     } while (0)
 
     for (nm_config_file_t *cf = files; cf; cf = cf->next) {
@@ -263,8 +252,8 @@ nm_config_t *nm_config_parse(nm_config_file_t *files, char **err_out) {
 
             char *s_typ = strtrim(strsep(&cur, ":"));
 
-            if (nm_config_parse__line_item(s_typ, &cur, &tmp_it, &tmp_act, &err)) {
-                if (err)
+            if (nm_config_parse__line_item(s_typ, &cur, &tmp_it, &tmp_act)) {
+                if ((err = nm_err()))
                     RETERR("file %s: line %d: parse menu_item: %s", cf->path, line_n, err);
                 if ((ret = nm_config_parse__append_item(&state, &tmp_it)))
                     RETERR("file %s: line %d: error appending item to config: %s", cf->path, line_n, nm_config_parse__strerror(ret));
@@ -273,16 +262,16 @@ nm_config_t *nm_config_parse(nm_config_file_t *files, char **err_out) {
                 continue;
             }
 
-            if (nm_config_parse__line_chain(s_typ, &cur, &tmp_act, &err)) {
-                if (err)
+            if (nm_config_parse__line_chain(s_typ, &cur, &tmp_act)) {
+                if ((err = nm_err()))
                     RETERR("file %s: line %d: parse chain: %s", cf->path, line_n, err);
                 if ((ret = nm_config_parse__append_action(&state, &tmp_act)))
                     RETERR("file %s: line %d: error appending action to config: %s", cf->path, line_n, nm_config_parse__strerror(ret));
                 continue;
             }
 
-            if (nm_config_parse__line_generator(s_typ, &cur, &tmp_gn, &err)) {
-                if (err)
+            if (nm_config_parse__line_generator(s_typ, &cur, &tmp_gn)) {
+                if ((err = nm_err()))
                     RETERR("file %s: line %d: parse generator: %s", cf->path, line_n, err);
                 if ((ret = nm_config_parse__append_generator(&state, &tmp_gn)))
                     RETERR("file %s: line %d: error appending generator to config: %s", cf->path, line_n, nm_config_parse__strerror(ret));
@@ -341,41 +330,35 @@ nm_config_t *nm_config_parse(nm_config_file_t *files, char **err_out) {
     if (rm > NM_CONFIG_MAX_MENU_ITEMS_PER_MENU)
         RETERR("too many menu items in reader menu (> %d)", NM_CONFIG_MAX_MENU_ITEMS_PER_MENU);
 
-    NM_RETURN_OK(state.cfg_s);
-    #undef NM_ERR_RET
+    return state.cfg_s;
 }
 
-static bool nm_config_parse__line_item(const char *type, char **line, nm_menu_item_t *it_out, nm_menu_action_t *action_out, char **err_out) {
-    #define NM_ERR_RET true
-
-    if (strcmp(type, "menu_item"))
-        NM_RETURN_OK(false);
+static bool nm_config_parse__line_item(const char *type, char **line, nm_menu_item_t *it_out, nm_menu_action_t *action_out) {
+    if (strcmp(type, "menu_item")) {
+        nm_err_set(NULL);
+        return false;
+    }
 
     *it_out = (nm_menu_item_t){0};
 
     char *s_loc = strtrim(strsep(line, ":"));
-    if      (!s_loc)                   NM_RETURN_ERR("field 2: expected location, got end of line");
+    if      (!s_loc)                   NM_ERR_RET(NULL, "field 2: expected location, got end of line");
     else if (!strcmp(s_loc, "main"))   it_out->loc = NM_MENU_LOCATION_MAIN_MENU;
     else if (!strcmp(s_loc, "reader")) it_out->loc = NM_MENU_LOCATION_READER_MENU;
-    else NM_RETURN_ERR("field 2: unknown location '%s'", s_loc);
+    else NM_ERR_RET(NULL, "field 2: unknown location '%s'", s_loc);
 
     char *p_lbl = strtrim(strsep(line, ":"));
-    if (!p_lbl) NM_RETURN_ERR("field 3: expected label, got end of line");
+    if (!p_lbl) NM_ERR_RET(NULL, "field 3: expected label, got end of line");
     it_out->lbl = p_lbl;
 
-    nm_config_parse__lineend_action(4, line, true, true, action_out, err_out);
-    if (*err_out)
-        return NM_ERR_RET;
-
-    NM_RETURN_OK(true);
-    #undef NM_ERR_RET
+    return nm_config_parse__lineend_action(4, line, true, true, action_out);
 }
 
-static bool nm_config_parse__line_chain(const char *type, char **line, nm_menu_action_t *act_out, char **err_out) {
-    #define NM_ERR_RET true
-
-    if (strncmp(type, "chain_", 5))
-        NM_RETURN_OK(false);
+static bool nm_config_parse__line_chain(const char *type, char **line, nm_menu_action_t *act_out) {
+    if (strncmp(type, "chain_", 5)) {
+        nm_err_set(NULL);
+        return false;
+    }
 
     bool p_on_success, p_on_failure;
     if (!strcmp(type, "chain_success")) {
@@ -387,70 +370,66 @@ static bool nm_config_parse__line_chain(const char *type, char **line, nm_menu_a
     } else if (!strcmp(type, "chain_failure")) {
         p_on_success = false;
         p_on_failure = true;
-    } else NM_RETURN_OK(false);
+    } else {
+        nm_err_set(NULL);
+        return false;
+    }
 
-    nm_config_parse__lineend_action(2, line, p_on_success, p_on_failure, act_out, err_out);
-    if (*err_out)
-        return NM_ERR_RET;
-
-    NM_RETURN_OK(true);
-    #undef NM_ERR_RET
+    return nm_config_parse__lineend_action(2, line, p_on_success, p_on_failure, act_out);
 }
 
-static bool nm_config_parse__line_generator(const char *type, char **line, nm_generator_t *gn_out, char **err_out) {
-    #define NM_ERR_RET true
-
-    if (strcmp(type, "generator"))
-        NM_RETURN_OK(false);
+static bool nm_config_parse__line_generator(const char *type, char **line, nm_generator_t *gn_out) {
+    if (strcmp(type, "generator")) {
+        nm_err_set(NULL);
+        return false;
+    }
 
     *gn_out = (nm_generator_t){0};
 
     char *s_loc = strtrim(strsep(line, ":"));
-    if      (!s_loc)                   NM_RETURN_ERR("field 2: expected location, got end of line");
+    if      (!s_loc)                   NM_ERR_RET(NULL, "field 2: expected location, got end of line");
     else if (!strcmp(s_loc, "main"))   gn_out->loc = NM_MENU_LOCATION_MAIN_MENU;
     else if (!strcmp(s_loc, "reader")) gn_out->loc = NM_MENU_LOCATION_READER_MENU;
-    else NM_RETURN_ERR("field 2: unknown location '%s'", s_loc);
+    else NM_ERR_RET(NULL, "field 2: unknown location '%s'", s_loc);
 
     char *s_generate = strtrim(strsep(line, ":"));
-    if (!s_generate) NM_RETURN_ERR("field 3: expected generator, got end of line");
+    if (!s_generate) NM_ERR_RET(NULL, "field 3: expected generator, got end of line");
     #define X(name) \
     else if (!strcmp(s_generate, #name)) gn_out->generate = NM_GENERATOR(name);
     NM_GENERATORS
     #undef X
-    else NM_RETURN_ERR("field 3: unknown generator '%s'", s_generate);
+    else NM_ERR_RET(NULL, "field 3: unknown generator '%s'", s_generate);
 
     char *p_arg = strtrim(*line); // note: optional
     if (p_arg) gn_out->arg = p_arg;
 
     gn_out->desc = s_generate;
 
-    NM_RETURN_OK(true);
-    #undef NM_ERR_RET
+    nm_err_set(NULL);
+    return true;
 }
 
-static bool nm_config_parse__lineend_action(int field, char **line, bool p_on_success, bool p_on_failure, nm_menu_action_t *act_out, char **err_out) {
-    #define NM_ERR_RET true
-
+static bool nm_config_parse__lineend_action(int field, char **line, bool p_on_success, bool p_on_failure, nm_menu_action_t *act_out) {
     *act_out = (nm_menu_action_t){0};
 
     char *s_act = strtrim(strsep(line, ":"));
-    if (!s_act) NM_RETURN_ERR("field %d: expected action, got end of line", field);
+    if (!s_act) NM_ERR_RET(true, "field %d: expected action, got end of line", field);
     #define X(name) \
     else if (!strcmp(s_act, #name)) act_out->act = NM_ACTION(name);
     NM_ACTIONS
     #undef X
-    else NM_RETURN_ERR("field %d: unknown action '%s'", field, s_act);
+    else NM_ERR_RET(true, "field %d: unknown action '%s'", field, s_act);
 
     // type: menu_item - field 5: argument
     char *p_arg = strtrim(*line);
-    if (!p_arg) NM_RETURN_ERR("field %d: expected argument, got end of line\n", field+1);
+    if (!p_arg) NM_ERR_RET(true, "field %d: expected argument, got end of line\n", field+1);
     act_out->arg = p_arg;
 
     act_out->on_success = p_on_success;
     act_out->on_failure = p_on_failure;
 
-    NM_RETURN_OK(true);
-    #undef NM_ERR_RET
+    nm_err_set(NULL);
+    return true;
 }
 
 static nm_config_parse__append__ret_t nm_config_parse__append_item(nm_config_parse__state_t *restrict state, nm_menu_item_t *const restrict it) {

.gitignore
@@ -23,6 +23,7 @@ compile_flags.txt
 /src/config.o
 /src/kfmon.o
 /src/action.o
+/src/util.o
 /src/dlhook.o
 /src/generator.o
 /src/action_c.o

Makefile
@@ -100,7 +100,7 @@ override GENERATED += KoboRoot.tgz
 src/libnm.so: override CFLAGS   += $(PTHREAD_CFLAGS) -fvisibility=hidden -fPIC
 src/libnm.so: override CXXFLAGS += $(PTHREAD_CFLAGS) $(QT5CORE_CFLAGS) $(QT5WIDGETS_CFLAGS) -fvisibility=hidden -fvisibility-inlines-hidden -fPIC
 src/libnm.so: override LDFLAGS  += $(PTHREAD_LIBS) $(QT5CORE_LIBS) $(QT5WIDGETS_LIBS) -ldl -Wl,-soname,libnm.so
-src/libnm.so: src/qtplugin.o src/init.o src/config.o src/dlhook.o src/failsafe.o src/menu.o src/kfmon.o src/action.o src/action_c.o src/action_cc.o src/generator.o src/generator_c.o
+src/libnm.so: src/qtplugin.o src/init.o src/config.o src/dlhook.o src/failsafe.o src/menu.o src/kfmon.o src/action.o src/action_c.o src/action_cc.o src/generator.o src/generator_c.o src/util.o
 
 override LIBRARIES += src/libnm.so
 override MOCS      += src/qtplugin.moc

src/action.c
@@ -30,7 +30,6 @@ _nm_action_result_fmt(toast,  NM_ACTION_RESULT_TYPE_TOAST);
 void nm_action_result_free(nm_action_result_t *res) {
     if (!res)
         return;
-
     if (res->msg)
         free(res->msg);
     free(res);

src/action.h
@@ -17,7 +17,10 @@ typedef struct {
     int skip; // for use by skip only
 } nm_action_result_t;
 
-typedef nm_action_result_t *(*nm_action_fn_t)(const char *arg, char **err);
+// nm_action_fn_t represents an action. On success, a nm_action_result_t is
+// returned and needs to be freed with nm_action_result_free. Otherwise, NULL is
+// returned and nm_err is set.
+typedef nm_action_result_t *(*nm_action_fn_t)(const char *arg);
 
 nm_action_result_t *nm_action_result_silent();
 nm_action_result_t *nm_action_result_msg(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
@@ -27,9 +30,9 @@ void nm_action_result_free(nm_action_result_t *res);
 #define NM_ACTION(name) nm_action_##name
 
 #ifdef __cplusplus
-#define NM_ACTION_(name) extern "C" nm_action_result_t *NM_ACTION(name)(const char *arg, char **err_out)
+#define NM_ACTION_(name) extern "C" nm_action_result_t *NM_ACTION(name)(const char *arg)
 #else
-#define NM_ACTION_(name) nm_action_result_t *NM_ACTION(name)(const char *arg, char **err_out)
+#define NM_ACTION_(name) nm_action_result_t *NM_ACTION(name)(const char *arg)
 #endif
 
 #define NM_ACTIONS    \

src/action_c.c
@@ -6,49 +6,38 @@
 #include "util.h"
 
 NM_ACTION_(dbg_syslog) {
-    #define NM_ERR_RET NULL
     NM_LOG("dbgsyslog: %s", arg);
-    NM_RETURN_OK(nm_action_result_silent());
-    #undef NM_ERR_RET
+    return nm_action_result_silent();
 }
 
 NM_ACTION_(dbg_error) {
-    #define NM_ERR_RET NULL
-    NM_RETURN_ERR("%s", arg);
-    #undef NM_ERR_RET
+    NM_ERR_SET("%s", arg);
+    return NULL;
 }
 
 NM_ACTION_(dbg_msg) {
-    #define NM_ERR_RET NULL
-    NM_RETURN_OK(nm_action_result_msg("%s", arg));
-    #undef NM_ERR_RET
+    return nm_action_result_msg("%s", arg);
 }
 
 NM_ACTION_(dbg_toast) {
-    #define NM_ERR_RET NULL
-    NM_RETURN_OK(nm_action_result_toast("%s", arg));
-    #undef NM_ERR_RET
+    return nm_action_result_toast("%s", arg);
 }
 
 NM_ACTION_(skip) {
-    #define NM_ERR_RET NULL
-
     char *tmp;
     long n = strtol(arg, &tmp, 10);
-    NM_ASSERT(*arg && !*tmp && n != 0 && n >= -1 && n < INT_MAX, "invalid count '%s': must be a nonzero integer or -1", arg);
+    NM_CHECK(NULL, *arg && !*tmp && n != 0 && n >= -1 && n < INT_MAX, "invalid count '%s': must be a nonzero integer or -1", arg);
 
     nm_action_result_t *res = calloc(1, sizeof(nm_action_result_t));
     res->type = NM_ACTION_RESULT_TYPE_SKIP;
     res->skip = (int)(n);
-    NM_RETURN_OK(res);
-
-    #undef NM_ERR_RET
+    return res;
 }
 
 NM_ACTION_(kfmon_id) {
     // Start by watch ID (simpler, but IDs may not be stable across a single power cycle, given severe KFMon config shuffling)
     int status = nm_kfmon_simple_request("start", arg);
-    return nm_kfmon_return_handler(status, err_out);
+    return nm_kfmon_return_handler(status);
 }
 
 NM_ACTION_(kfmon) {
@@ -56,9 +45,8 @@ NM_ACTION_(kfmon) {
     int status = nm_kfmon_simple_request("trigger", arg);
 
     // Fixup INVALID_ID to INVALID_NAME for slightly clearer feedback (see e8b2588 for details).
-    if (status == KFMON_IPC_ERR_INVALID_ID) {
+    if (status == KFMON_IPC_ERR_INVALID_ID)
         status = KFMON_IPC_ERR_INVALID_NAME;
-    }
 
-    return nm_kfmon_return_handler(status, err_out);
+    return nm_kfmon_return_handler(status);
 }

src/config.h
@@ -19,24 +19,25 @@ typedef struct nm_config_t nm_config_t;
 
 typedef struct nm_config_file_t nm_config_file_t;
 
-// nm_config_parse lists the configuration files in /mnt/onboard/.adds/nm. An
-// error is returned if there are errors reading the dir.
-nm_config_file_t *nm_config_files(char **err_out);
+// nm_config_parse lists the configuration files in /mnt/onboard/.adds/nm. If
+// there are errors reading the dir, NULL is returned and nm_err is set.
+nm_config_file_t *nm_config_files();
 
 // nm_config_files_update checks if the configuration files are up to date and
-// updates them, returning true, if not. If *files is NULL, it is equivalent to
-// doing `*files = nm_config_files(err_out)`. If an error occurs, the pointer is
-// left untouched and false is returned along with the error. Warning: if the
-// files have changed, the pointer passed to files will become invalid (it gets
-// replaced).
-bool nm_config_files_update(nm_config_file_t **files, char **err_out);
+// updates them. If the files are already up-to-date, 1 is returned. If the
+// files were updated, 0 is returned. If an error occurs, the pointer is left
+// untouched and -1 is returned with nm_err set. Warning: if the files have
+// changed, the pointer passed to files will become invalid (it gets replaced).
+// If *files is NULL, it is equivalent to doing `*files = nm_config_files()`.
+int nm_config_files_update(nm_config_file_t **files);
 
 // nm_config_files_free frees the list of configuration files.
 void nm_config_files_free(nm_config_file_t *files);
 
-// nm_config_parse parses the configuration files. An error is returned if there
-// are syntax errors, file access errors, or invalid action names for menu_item.
-nm_config_t *nm_config_parse(nm_config_file_t *files, char **err_out);
+// nm_config_parse parses the configuration files. If there are syntax errors,
+// file access errors, or invalid action names for menu_item, then NULL is
+// returned and nm_err is set. On success, the config is returned.
+nm_config_t *nm_config_parse(nm_config_file_t *files);
 
 // nm_config_generate runs all generators synchronously and sequentially. Any
 // previously generated items are automatically removed if updates are required.

commit 2cfb51ac0d64b425ae35c4971e6fa0a96159ea34

Fixed minor memory regression in #62
The line was being freed before the error is set.

Fixes 6329328daf09b50e18bbd4dad35cc7e8e36f7f78.

src/config.c
@@ -226,12 +226,13 @@ nm_config_t *nm_config_parse(nm_config_file_t *files) {
     nm_menu_action_t tmp_act;
     nm_generator_t   tmp_gn;
 
-    #define RETERR(fmt, ...) do {                  \
-        if (cfgfile)                               \
-            fclose(cfgfile);                       \
-        free(line);                                \
-        nm_config_free(state.cfg_c);               \
-        NM_ERR_RET(NULL, fmt, ##__VA_ARGS__);      \
+    #define RETERR(fmt, ...) do {       \
+        NM_ERR_SET(fmt, ##__VA_ARGS__); \
+        if (cfgfile)                    \
+            fclose(cfgfile);            \
+        free(line);                     \
+        nm_config_free(state.cfg_c);    \
+        return NULL;                    \
     } while (0)
 
     for (nm_config_file_t *cf = files; cf; cf = cf->next) {