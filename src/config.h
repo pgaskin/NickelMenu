c14b7d18426d859f310edd93fd4b0497f3c5b337
Patrick Gaskin <patrick@pgaskin.net>
2020-08-05 00:50:31 -0400

    Implemented browser menu location

--- src/config.h
+++ src/config.h
@@ -10,8 +10,9 @@ extern "C" {
 #include "action.h"
 
 typedef enum {
-    NM_MENU_LOCATION_MAIN_MENU   = 1,
-    NM_MENU_LOCATION_READER_MENU = 2,
+    NM_MENU_LOCATION_MAIN_MENU    = 1,
+    NM_MENU_LOCATION_READER_MENU  = 2,
+    NM_MENU_LOCATION_BROWSER_MENU = 3,
 } nm_menu_location_t;
 
 typedef struct nm_menu_action_t {
