9cca58207aa94840b660cdb4de26c597e6243eaa
redphx <96280+redphx@users.noreply.github.com>
2025-11-18 07:41:51 +0700

    feat: add support for new BluetoothManager::requestTurnOn in 4.44.23552

--- src/action_cc.cc
+++ src/action_cc.cc
@@ -924,9 +924,13 @@ NM_ACTION_(nickel_bluetooth) {
     uint (*BluetoothManager_up)(BluetoothManager *);
     NM_ACT_XSYM(BluetoothManager_up, "_ZNK16BluetoothManager2upEv", "could not dlsym BluetoothManager::up");
 
-    //libnickel 4.34.20097 * _ZN16BluetoothManager2onEv
+    //libnickel 4.44.23552 * _ZN16BluetoothManager13requestTurnOnEv
     void (*BluetoothManager_on)(BluetoothManager *);
-    NM_ACT_XSYM(BluetoothManager_on, "_ZN16BluetoothManager2onEv", "could not dlsym BluetoothManager::on");
+    NM_ACT_SYM(BluetoothManager_on, "_ZN16BluetoothManager13requestTurnOnEv");
+    //libnickel 4.34.20097 * _ZN16BluetoothManager2onEv
+    void (*BluetoothManager_onLegacy)(BluetoothManager *);
+    NM_ACT_SYM(BluetoothManager_onLegacy, "_ZN16BluetoothManager2onEv");
+    NM_CHECK(nullptr, BluetoothManager_on || BluetoothManager_onLegacy, "could not dlsym BluetoothManager::requestTurnOn");
 
     //libnickel 4.34.20097 * _ZN16BluetoothManager4scanEv
     void (*BluetoothManager_scan)(BluetoothManager *);
@@ -951,7 +955,11 @@ NM_ACTION_(nickel_bluetooth) {
         case CHECK:
             return nm_action_result_toast("Bluetooth is %s.", isUp ? "on" : "off");
         case ENABLE:
-            BluetoothManager_on(btm);
+            if (BluetoothManager_on) {
+                BluetoothManager_on(btm);
+            } else if (BluetoothManager_onLegacy) {
+                BluetoothManager_onLegacy(btm);
+            }
             BluetoothManager_scan(btm);
             return nm_action_result_toast("Bluetooth turned on.");
         case DISABLE:
